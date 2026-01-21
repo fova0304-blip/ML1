82e898367b21c864b27c060dcbcecd751a29f1af
fova0304 <fova0304@gmail.com>
2026-01-22 00:25:52 +0900

    데이터 로드 및 전처리 및 model 완성

---
+++ train/train_model.py
@@ -0,0 +1,25 @@
+import pandas as pd 
+import numpy as np 
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+
+# 데이터 로딩후, 모델훈련전 나누기
+df = pd.read_csv("cardio_train.csv",sep=";")
+X = df.drop("cardio",axis=1)
+y = df["cardio"]
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf = RandomForestClassifier(
+    n_estimators=400,
+    max_depth=None,
+    min_samples_split=2,
+    min_samples_leaf=1,
+    max_features="sqrt",
+    n_jobs=-1,
+    random_state=42,
+    class_weight="balanced",
+)
+rf.fit(X_train, y_train)
+print(rf.score(X_test, y_test))
