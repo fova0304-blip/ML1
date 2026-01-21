0436a48472ae7ecdc157fe1f96a752b5f1e26547
fova0304 <fova0304@gmail.com>
2026-01-22 02:04:52 +0900

    main.py added and done

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from fastapi import FastAPI 
+from model import proba_result
+from schemas import PredictRequest, PredictResponse
 
 app = FastAPI()
 
-@app.post("/health")
-def health_check_api():
-    return 
+@app.post("/check")
+def check_api():
+    return {
+        "test": "good"
+    }
 
 @app.post("/predict")
-def predict_api():
-    return 
+def predict_api(body:PredictRequest)->PredictResponse:
+    # health_status={
+    #     "age": body.age,
+    #     "gender": body.gender,
+    #     "height": body.height,
+    #     "weight": body.weight,
+    #     "ap_hi": body.ap_hi,
+    #     "ap_lo": body.ap_lo,
+    #     "cholesterol": body.cholesterol,
+    #     "gluc": body.gluc,
+    #     "smoke": body.smoke,
+    #     "alco": body.alco,
+    #     "active": body.active,
+    # } 
+    proba,result = proba_result(body)
+    return {
+        "probability": proba,
+        "result": result
+    }
