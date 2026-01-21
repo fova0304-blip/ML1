a527d0046955867da0adb64cb4aacbe9604e5507
fova0304 <fova0304@gmail.com>
2026-01-22 00:57:03 +0900

    joblib model saved in model folder and got added on gitignore due to size of the model

--- train/train_model.py
+++ train/train_model.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np 
 from sklearn.model_selection import train_test_split,GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
+import os
+import joblib
 
 # 데이터 로딩후, 모델훈련전 나누기
 df = pd.read_csv("cardio_train.csv",sep=";")
@@ -9,8 +11,8 @@ X = df.drop("cardio",axis=1)
 y = df["cardio"]
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-from sklearn.ensemble import RandomForestClassifier
 
+# 모델 파라미터 설정후, 모델 저장해줌
 rf = RandomForestClassifier(
     n_estimators=400,
     max_depth=None,
@@ -23,3 +25,6 @@ rf = RandomForestClassifier(
 )
 rf.fit(X_train, y_train)
 print(rf.score(X_test, y_test))
+
+os.makedirs("model",exist_ok=True)
+joblib.dump(rf, "model/train_model.joblib")
